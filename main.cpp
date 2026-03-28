e1332772ff01e7d9552cea18cc51c3f450374c52
Martyn Davis <mcd@marengo.cc>
2026-03-28 11:12:41 +0000

    Added cpp-terminal as FetchContent

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
-#include <print>
+#include "cpp-terminal/color.hpp"
+#include "cpp-terminal/cursor.hpp"
+#include "cpp-terminal/screen.hpp"
+
+#include <iostream>
 
 int main() {
-    std::println("Hello world");
+    std::cout << Term::clear_screen();
+    std::cout << Term::cursor_move(2,2) <<
+        Term::color_fg(Term::Color::Name::Red) << "Hello world\n" << std::endl;
+    return 0;
 }
