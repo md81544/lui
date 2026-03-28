4451a327ad43e74d7927e8bea167587625f74038
Martyn Davis <mcd@marengo.cc>
2026-03-28 22:41:59 +0000

    Missed files from previous commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-#include "cpp-terminal/color.hpp"
-#include "cpp-terminal/cursor.hpp"
-#include "cpp-terminal/screen.hpp"
-
+#include "terminal.h"
 #include <iostream>
 
-int main() {
-    std::cout << Term::clear_screen();
-    std::cout << Term::cursor_move(2,2) <<
-        Term::color_fg(Term::Color::Name::Red) << "Hello ";
-    // Reset the fg colour to the terminal's default
-    std::cout << Term::color_fg(Term::Color::Name::Default ) << "world\n" << std::endl;
+int main()
+{
+    terminal::Terminal term;
+    term.setFgColour(terminal::Colour::BrightRed);
+    term.printAt(2, 2, "Hello world");
+    term.setFgColour(terminal::Colour::BrightWhite);
+    term.printAt(4, 4, "All your base are belong to us");
+    term.setFgColour(terminal::Colour::Grey);
+    term.printAt(5,5, "This is grey");
+    term.setFgColour(terminal::Colour::Default);
+    term.render();
+    std::cout << "\nPress a key " << std::flush;
+    [[maybe_unused]] char c = term.getChar();
     return 0;
 }
