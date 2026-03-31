a52ee4fc774932df7d207581d70aa2e6a9a49083
Martyn Davis <mcd@marengo.cc>
2026-03-31 09:25:35 +0100

    First rough (WIP) proper UI interface

--- ui.cpp
+++ ui.cpp
@@ -1,9 +1,11 @@
 #include "ui.h"
 #include "keypress.h"
+#include "terminal.h"
 #include "word_searcher.h"
 #include <filesystem>
 #include <format>
 #include <memory>
+#include <stdexcept>
 
 namespace {
 std::filesystem::path locateDataDirectory(std::string_view argv0)
@@ -37,78 +39,76 @@ Ui::Ui(std::string_view argv0)
 
 int Ui::run()
 {
-    // This is currently just a test for the Terminal
-    // class which formats output (and reads key presses)
-
-    std::size_t row = 10;
-
-    m_term.cursorOn();
-
-    while (true) {
-
-        m_term.setFgColour(terminal::Colour::BrightRed);
-        m_term.printAt(2, 2, "Hello world");
-        m_term.setFgColour(terminal::Colour::BrightWhite);
-        m_term.printAt(4, 4, "All your base are belong to us");
-        m_term.setFgColour(terminal::Colour::Grey);
-        m_term.printAt(5, 5, "This is grey");
-        m_term.setFgColour(terminal::Colour::BrightMagenta);
-        m_term.printAt(row, 18, "This is movable (up/down arrow)");
-
-        // Fixed position footer
-        m_term.setFgColour(terminal::Colour::Default);
+    bool finished { false };
+    while (!finished) {
+        // Check for terminal resize
         auto [rows, cols] = m_term.getTerminalSize();
-        std::string hr;
-        for (std::size_t c = 0; c < cols; ++c) {
-            if (m_term.utf8Supported()) {
-                hr.append("─"); // UTF-8 line
-            } else {
-                hr.append("-");
-            }
+        if (rows < 10 || cols < 20) { // TODO tweak this accordingly when layout is finalised
+            throw(std::runtime_error("Terminal size is too small!"));
         }
-
-        m_term.printAt(rows - 3, 0, hr);
-        m_term.printAt(rows - 2, 0, std::format("Terminal size: {} rows, and {} cols", rows, cols));
-
-        // Helper function to highlight items in string for menus
-        m_term.goTo(12, 0);
-        m_term.printMenuString(
-            terminal::Colour::Yellow,
-            terminal::Colour::BrightWhite,
-            "_Lookup _Edit _Selection _View _Help");
-        m_term.print("\n");
-        m_term.setFgColour(terminal::Colour::Default);
-
-        m_term.print("\nSelect option (Esc to quit) ");
-
+        m_rows = rows;
+        m_cols = cols;
+        displayHeader();
+        displayMenu();
         m_term.render();
-
         int keyPress = m_term.getChar();
-        if (keyPress == keyPress::ESC || keyPress == keyPress::CTRL_C) {
-            break;
-        } else if (keyPress == keyPress::UP) {
-            if (row > 0) {
-                --row;
-            }
-        } else if (keyPress == keyPress::DOWN) {
-            if (row < rows - 4) {
-                ++row;
-            }
-        } else if (keyPress == keyPress::CTRL_U) {
-            m_term.setBgColour(terminal::Colour::BrightGreen);
-            m_term.setFgColour(terminal::Colour::Black);
-            m_term.printAt(rows - 4, 0, "Ctrl-U was pressed");
-            m_term.setBgColour(terminal::Colour::Default);
-        } else if (keyPress == 'l' || keyPress == 'L') {
-            auto vec = m_ws->regexSearch("c.m..t.r");
-            m_term.goTo(16, 0);
-            m_term.setFgColour(terminal::Colour::BrightGreen);
-            for (const auto& w : vec) {
-                m_term.print(std::format("{} ", w));
-            }
-        } else {
-            m_term.bell();
+        switch (keyPress) {
+            case keyPress::CTRL_C:
+            case 'Q':
+            case 'q':
+                finished = true;
+                break;
+            default:
+                m_term.bell();
         }
     }
     return 0;
+}
+
+void Ui::displayHeader()
+{
+    m_term.printAt(1, 1, std::format("Search:  {}", m_searchString));
+    m_term.printAt(2, 1, std::format("Found:   {}", m_foundString));
+    m_term.printAt(3, 1, std::format("Comment: {}", m_comment));
+    m_term.printAt(4, 1, std::format("Clue:    {}", m_clue));
+    hr(5);
+}
+
+void Ui::displayMenu()
+{
+    hr(m_rows - 3);
+    m_term.printAt(m_rows - 3, 1, "Menu");
+    m_term.goTo(m_rows - 2, 1);
+    m_term.printMenuString(
+        terminal::Colour::Default,
+        terminal::Colour::BrightWhite,
+        "_Jumble _Found _Remove _Comment re_Verse re_Gular _Thesaurus");
+    m_term.goTo(m_rows - 1, 1);
+    m_term.printMenuString(
+        terminal::Colour::Default,
+        terminal::Colour::BrightWhite,
+        "_Anagram _Lookup _Define _Note st_Ore r_Etrieve re_Start _Quit");
+}
+
+void Ui::restart()
+{
+    m_searchString.clear();
+    m_foundString.clear();
+    m_clue.clear();
+    m_comment.clear();
+    m_results.clear();
+}
+
+void Ui::hr(std::size_t row)
+{
+    m_term.goTo(row, 0);
+    std::string hr;
+    for (std::size_t c = 0; c < m_cols; ++c) {
+        if (m_term.utf8Supported()) {
+            hr.append("─"); // UTF-8 line character
+        } else {
+            hr.append("-"); // plain
+        }
+    }
+    m_term.print(hr);
 }
--- ui.h
+++ ui.h
@@ -7,8 +7,10 @@
 
 #include "terminal.h"
 #include "word_searcher.h"
+#include <cstddef>
 #include <memory>
 #include <string_view>
+#include <vector>
 
 class Ui final {
 public:
@@ -16,6 +18,18 @@ public:
     ~Ui() { };
     int run(); // main application run loop
 private:
+    void displayHeader();
+    void displayMenu();
+    void restart();
+    void hr(std::size_t row);
+
     terminal::Terminal m_term;
+    std::size_t m_rows;
+    std::size_t m_cols;
     std::unique_ptr<WordSearcher> m_ws;
+    std::string m_searchString;
+    std::string m_foundString;
+    std::string m_clue;
+    std::string m_comment;
+    std::vector<std::string> m_results;
 };
