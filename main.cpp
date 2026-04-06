a0164d69309c90f2224ab3fdebe07f6be0dc4609
Martyn Davis <mcd@marengo.cc>
2026-04-06 07:34:39 +0100

    Init log with min Debug level in debug build, otherwise Info

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 
 int main(int, char** argv)
 {
+#ifndef NDEBUG
     mgo::Log::init("lui.log", mgo::Log::Level::Debug);
+    mgo::Log::info("Running DEBUG build");
+#else
+    mgo::Log::init("lui.log", mgo::Log::Level::Info);
+    mgo::Log::info("Running release build");
+#endif 
     mgo::Log::info("Lui started");
     try {
         ui::Ui ui(argv[0]);
