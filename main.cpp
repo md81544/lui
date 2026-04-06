958ec10cba7bb4aedd108d762c04c649be8d6237
Martyn Davis <mcd@marengo.cc>
2026-04-06 06:46:36 +0100

    Adds a proper logger

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+#include "log.h"
 #include "ui.h"
 #include <iostream>
 
 int main(int, char** argv)
 {
+    mgo::Log::init("lui.log", mgo::Log::Level::Debug);
+    mgo::Log::info("Lui started");
     try {
         ui::Ui ui(argv[0]);
         return ui.run();
