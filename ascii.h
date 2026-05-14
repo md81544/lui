5680b8252f30e661d45bf397da969b421156ecf9
Martyn Davis <mcd@marengo.cc>
2026-05-14 12:26:57 +0100

    Converts ascii:: functions to take ints instead of unsigned chars to avoid -fsanitize errors

--- ascii.h
+++ ascii.h
@@ -3,75 +3,77 @@
 // Note, these are locale-independent, used deliberately
 // to avoid locale issues if the user's locale is set
 // to something other than expected
+// The functions take ints to cope with extended character input
+// without sanitisation errors
 
 namespace ascii {
 
-constexpr bool isascii(unsigned char c) noexcept
+constexpr bool isascii(int c) noexcept
 {
     return c <= 0x7F;
 }
 
-constexpr bool isdigit(unsigned char c) noexcept
+constexpr bool isdigit(int c) noexcept
 {
     return c >= '0' && c <= '9';
 }
 
-constexpr bool islower(unsigned char c) noexcept
+constexpr bool islower(int c) noexcept
 {
     return c >= 'a' && c <= 'z';
 }
 
-constexpr bool isupper(unsigned char c) noexcept
+constexpr bool isupper(int c) noexcept
 {
     return c >= 'A' && c <= 'Z';
 }
 
-constexpr bool isalpha(unsigned char c) noexcept
+constexpr bool isalpha(int c) noexcept
 {
     return islower(c) || isupper(c);
 }
 
-constexpr bool isalnum(unsigned char c) noexcept
+constexpr bool isalnum(int c) noexcept
 {
     return isalpha(c) || isdigit(c);
 }
 
-constexpr bool isxdigit(unsigned char c) noexcept
+constexpr bool isxdigit(int c) noexcept
 {
     return isdigit(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
 }
 
-constexpr bool isspace(unsigned char c) noexcept
+constexpr bool isspace(int c) noexcept
 {
     return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v';
 }
 
-constexpr bool isprint(unsigned char c) noexcept
+constexpr bool isprint(int c) noexcept
 {
     return c >= 0x20 && c <= 0x7E;
 }
 
-constexpr bool iscntrl(unsigned char c) noexcept
+constexpr bool iscntrl(int c) noexcept
 {
     return c <= 0x1F || c == 0x7F;
 }
 
-constexpr char tolower(unsigned char c) noexcept
+constexpr char tolower(int c) noexcept
 {
     return isupper(c) ? static_cast<char>(c + ('a' - 'A')) : static_cast<char>(c);
 }
 
-constexpr char toupper(unsigned char c) noexcept
+constexpr char toupper(int c) noexcept
 {
     return islower(c) ? static_cast<char>(c - ('a' - 'A')) : static_cast<char>(c);
 }
 
-constexpr bool iequal(unsigned char a, unsigned char b) noexcept
+constexpr bool iequal(int a, int b) noexcept
 {
     return tolower(a) == tolower(b);
 }
 
-constexpr bool ispunct(unsigned char c)
+constexpr bool ispunct(int c)
 {
     if (!isprint(c)) {
         return false;
