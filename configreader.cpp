a89f8668bc1bef639667d35e99e6afc208411287
Martyn Davis <mcd@marengo.cc>
2026-05-17 11:49:32 +0100

    Adds a basic YAML-like config reader (WIP: not used yet)

--- configreader.cpp
+++ configreader.cpp
@@ -3,111 +3,127 @@
 #include "utils.h"
 
 #include <algorithm>
+#include <cassert>
+#include <charconv>
+#include <format>
 #include <fstream>
+#include <stdexcept>
+#include <string_view>
+
+namespace {
+
+std::optional<double> isNumeric(std::string_view s)
+{
+    if (s.empty()) {
+        return std::nullopt;
+    }
+    std::string stripped;
+    stripped.reserve(s.size());
+    std::copy_if(s.begin(), s.end(), std::back_inserter(stripped), [](char c) { return c != ','; });
+    const char* first = stripped.data();
+    const char* last = stripped.data() + stripped.size();
+    double val;
+    auto [p, e] = std::from_chars(first, last, val);
+    if (e == std::errc {} && p == last) {
+        return val;
+    }
+    return std::nullopt;
+}
+
+} // anonymous namespace
 
 namespace mgo {
 
-ConfigReader::ConfigReader(const std::string& cfgfile)
+ConfigReader::ConfigReader(std::string_view cfgfile)
 {
     // Open the config file
     std::ifstream ifs;
-    ifs.open(cfgfile.c_str(), std::ios::in);
+    ifs.open(cfgfile.data(), std::ios::in);
     if (!ifs) {
-        throw std::runtime_error("Could not open " + cfgfile);
+        throw std::runtime_error(std::format("Could not open {}", cfgfile));
     }
     std::string line;
     size_t n;
+    std::string outerKey;
     while (getline(ifs, line)) {
+        bool indented = false;
+        if (line.size() > 0 && ascii::isspace(line[0])) {
+            indented = true;
+        }
         utils::trim(line);
-        if (line != "" && line[0] != '#') {
+        if (!line.empty() && line[0] != '#') {
             n = line.find(":");
             if (n > 0) {
                 std::string key = line.substr(0, n);
                 utils::trim(key);
                 std::transform(key.begin(), key.end(), key.begin(), ascii::toupper);
+                if (!indented) {
+                    outerKey = key;
+                }
                 std::string value = line.substr(n + 1);
                 utils::trim(value);
-                m_map[key] = value;
+                if (indented) {
+                    key = outerKey + "/" + key;
+                }
+                if (!value.empty()) {
+                    if (value == "true") {
+                        m_map[key] = true;
+                    } else if (value == "false") {
+                        m_map[key] = false;
+                    } else if (auto val = isNumeric(value)) {
+                        m_map[key] = *val;
+                    } else {
+                        m_map[key] = value;
+                    }
+                }
             }
         }
     }
     ifs.close();
 }
 
-std::string ConfigReader::read(const std::string& key, const std::string& defaultValue) const
+CfgValueType ConfigReader::read(std::string_view key, const CfgValueType& defaultValue) const
 {
-    std::string upperKey = key;
-    std::string value;
-    utils::trim(upperKey);
-    std::transform(upperKey.begin(), upperKey.end(), upperKey.begin(), ascii::toupper);
-    auto it = m_map.find(upperKey);
-    if (it == m_map.end()) {
-        value = defaultValue;
-    } else {
-        value = it->second;
-        if (value.empty()) {
-            value = defaultValue;
-        }
-    }
-    // Finally - if the value contains the string ${...} then we
-    // should substitute its contents for another variable from the
-    // config file.
-    size_t pos;
-    while ((pos = value.find("${")) != std::string::npos) {
-        size_t endPos = value.find("}", pos);
-        if (endPos == std::string::npos) {
-            break; // no terminating brace so return string as is
-        }
-        std::string key = value.substr(pos + 2, (endPos - pos) - 2);
-        value = value.substr(0, pos) + read(key, "") + value.substr(endPos + 1);
-    }
-    return value;
-}
-
-unsigned long ConfigReader::readLong(const std::string& key, unsigned long defaultValue) const
-{
-    auto it = m_mapLong.find(key);
-    if (it != m_mapLong.end()) {
+    std::string k(key);
+    std::transform(k.begin(), k.end(), k.begin(), ascii::toupper);
+    auto it = m_map.find(k.data());
+    if (it != m_map.end()) {
         return it->second;
     }
-    long rc = std::stoul(read(key, std::to_string(defaultValue)));
-    m_mapLong.insert({ key, rc });
-    return rc;
+    return defaultValue;
 }
 
-double ConfigReader::readDouble(const std::string& key, double defaultValue) const
+double ConfigReader::readDouble(std::string_view key, double defaultValue) const
 {
-    auto it = m_mapDouble.find(key);
-    if (it != m_mapDouble.end()) {
-        return it->second;
+    try {
+        auto val = std::get<double>(read(key, defaultValue));
+        return val;
+    } catch (std::bad_variant_access const&) {
+        assert(false);
+        return defaultValue;
     }
-    double rc = std::stod(read(key, std::to_string(defaultValue)));
-    m_mapDouble.insert({ key, rc });
-    return rc;
 }
 
-bool ConfigReader::readBool(const std::string& key, bool defaultBoolValue) const
+bool ConfigReader::readBool(std::string_view key, bool defaultValue) const
 {
-    auto it = m_mapBool.find(key);
-    if (it != m_mapBool.end()) {
-        return it->second;
+    try {
+        auto val = std::get<bool>(read(key, defaultValue));
+        return val;
+    } catch (std::bad_variant_access const&) {
+        assert(false);
+        return defaultValue;
     }
-    std::string defaultValue;
-    if (defaultBoolValue == true) {
-        defaultValue = "Y";
-    } else {
-        defaultValue = "N";
-    }
-    std::string value = read(key, defaultValue);
-    std::transform(value.begin(), value.end(), value.begin(), ascii::toupper);
-    bool rc;
-    if (value[0] == 'Y' || value[0] == 'T') {
-        rc = true;
-    } else {
-        rc = false;
-    }
-    m_mapBool.insert({ key, rc });
-    return rc;
 }
 
-} // namespace mgo
+std::string ConfigReader::readString(std::string_view key, const std::string& defaultValue) const
+{
+    try {
+        auto val = std::get<std::string>(read(key, defaultValue));
+        return val;
+    } catch (std::bad_variant_access const&) {
+        assert(false);
+        return defaultValue;
+    }
+}
+
+} // mgo
--- configreader.h
+++ configreader.h
@@ -1,59 +1,81 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <unordered_map>
+#include <variant>
+
+// NOTE! This supports a VERY simplified YAML sytax.
+// All that it supports is key : value pairs,
+// and SINGLE-LEVEL blocks, which can be indented by any number of
+// spaces.
+// For example a config file might look like:
+//
+// foo : 23
+// bar:
+//   baz: true
+//
+// The caller can call readDouble("foo") to get 23
+// Or readBool("bar/baz") to get true.
+//
+// Note: if the wrong type is attempted (e.g. readBool on a numeric value)
+// then an assertion will occur in debug mode, and the default value will
+// be returned in release mode.
 
 namespace mgo {
 
+using CfgValueType = std::variant<double, bool, std::string>;
+
 class IConfigReader {
 public:
-    virtual std::string read(const std::string& key, const std::string& defaultValue = "") const
-        = 0;
-    virtual unsigned long readLong(const std::string& key, unsigned long defaultValue = 0) const
-        = 0;
-    virtual double readDouble(const std::string& key, double defaultValue = 0.0) const = 0;
-    virtual bool readBool(const std::string& key, bool defaultValue) const = 0;
+    virtual double readDouble(std::string_view key, double defaultValue) const = 0;
+    virtual bool readBool(std::string_view key, bool defaultValue) const = 0;
+    virtual std::string readString(std::string_view key, const std::string& defaultValue) const = 0;
+
+private:
+    virtual CfgValueType read(std::string_view key, const CfgValueType& defaultValue) const = 0;
 };
 
-// Note, default parameters are statically bound, but I'm leaving them
-// all on the derived classes for reference. They should never need to change.
-
+// Mock config reader always returns the defaultValue
 class MockConfigReader final : public IConfigReader {
-    std::string read(const std::string&, const std::string& defaultValue = "") const override
-    {
+public:
+    virtual double readDouble(std::string_view, double defaultValue) const override {
         return defaultValue;
     }
-    unsigned long readLong(const std::string&, unsigned long defaultValue = 0) const override
-    {
+    virtual bool readBool(std::string_view, bool defaultValue) const override {
         return defaultValue;
     }
-    double readDouble(const std::string&, double defaultValue = 0.0) const override
-    {
+    virtual std::string readString(std::string_view, const std::string& defaultValue) const override {
         return defaultValue;
     }
-    bool readBool(const std::string&, bool defaultValue) const override
-    {
+
+private:
+    // This is unused
+    virtual CfgValueType read(std::string_view, const CfgValueType& defaultValue) const override {
         return defaultValue;
     }
 };
 
 class ConfigReader final : public IConfigReader {
 public:
-    ConfigReader(const std::string& sConfigFileName);
+    ConfigReader(std::string_view sConfigFileName);
     ~ConfigReader() { }
-    std::string read(const std::string& key, const std::string& defaultValue = "") const override;
-    unsigned long readLong(const std::string& key, unsigned long defaultValue = 0) const override;
-    double readDouble(const std::string& key, double defaultValue = 0.0) const override;
-    bool readBool(const std::string& key, bool defaultValue) const override;
+    // Cannot templatize virtual functions, hence the following "specializations"
+    // which suffice for our needs. If ConfigReader did not inherit from IConfigReader we
+    // could templatize, but then we'd lose the ability to mock it out. This approach
+    // avoids the caller needing to use std::get on a variant result. See notes above -
+    // these functions will assert(false) if the wrong type is attempted to be read in
+    // debug mode, and the default will be silently returned in release mode.
+    virtual double readDouble(std::string_view key, double defaultValue) const override;
+    virtual bool readBool(std::string_view key, bool defaultValue) const override;
+    virtual std::string readString(std::string_view key, const std::string& defaultValue) const override;
 
 private:
-    std::unordered_map<std::string, std::string> m_map;
-    // Lazy caches for bool, double, and long types.
-    // We could use a map of variants, but as we only
-    // support three types, it's simple enough like this.
-    mutable std::unordered_map<std::string, bool> m_mapBool;
-    mutable std::unordered_map<std::string, double> m_mapDouble;
-    mutable std::unordered_map<std::string, long> m_mapLong;
+    virtual CfgValueType
+    read(std::string_view key, const CfgValueType& defaultValue) const override;
+
+private:
+    std::unordered_map<std::string, CfgValueType> m_map;
 };
 
 } // namespace mgo
